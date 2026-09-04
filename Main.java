d9b0a6028a0771a11d9cb36acf1fc8a47586a21c
Jesse <admjes003@myuct.ac.za>
2026-09-04 19:41:53 +0200

    Added distance calculator

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 package FrequencyAllocation;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        
+    }
 }
--- src/Graph.java
+++ src/Graph.java
@@ -2,4 +2,10 @@ package FrequencyAllocation.src;
 
 public class Graph {
     
+    public double distanceCalculator(Cell cell1, Cell cell2) {
+        double eastdiff = cell2.easting - cell1.easting;
+        double northdiff = cell2.northing - cell1.northing;
+
+        return Math.sqrt((eastdiff * eastdiff) + (northdiff * northdiff));
+    }
 }

1eb4d7bd26a35c2541dff832afb00b69c45aca43
Jesse <admjes003@myuct.ac.za>
2026-09-06 23:12:36 +0200

    Added comments to FrequencyAllocator class

--- src/FrequencyAllocator.java
+++ src/FrequencyAllocator.java
@@ -4,14 +4,9 @@
 import java.util.*;
 public class FrequencyAllocator {
 
-    int bestCount;
     Map<String, Integer> bestAllocation = new HashMap<>();
 
-    public FrequencyAllocator(int bestCount) {
-        this.bestCount = bestCount;
-
-    }
-
+    // Checks that the current frequency can be assigned to the current cell.
     public boolean isFrequencySafe(Cell currentCell, Map<Cell, List<Cell>> network, int currentFrequency) {
         List<Cell> closeCells = network.get(currentCell);
 
@@ -23,12 +18,21 @@ public class FrequencyAllocator {
         return true;
     }
 
+    /* Uses backtracking to find a valid frequency allocations.
+       maxFrequencies limits the amount of frequencies being used.
+     */
     public boolean allocateFrequencies(Cell[] cells, Map<Cell, List<Cell>> network, int currentIndex, int numCells, int[] frequencies, int maxFrequencies) {
+
+        // All cells have been successfully assigned a frequency.
         if (currentIndex == numCells) {
             return true;
         }
 
+        // Continue assigning frequencies to the remaining cells.
+        // If this choice eventually fails, backtrack and try another frequency.
         for (int i = 0; i < maxFrequencies; i++) {
+
+            // Frequency is safe to use.
             if (isFrequencySafe(cells[currentIndex], network, frequencies[i])) {
                 cells[currentIndex].frequency = frequencies[i];
 
@@ -37,7 +41,7 @@ public class FrequencyAllocator {
                 if (success) {
                     return true;
                 }
-            
+            // Backtrack.
             cells[currentIndex].frequency = 0;
             }
         }

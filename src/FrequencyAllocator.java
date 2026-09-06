717b18e301880dddc122566098f9995dbee9b392
Jesse <admjes003@myuct.ac.za>
2026-09-06 16:39:03 +0200

    Edited isFrequencySafe to compare to current frequency being considered

--- src/FrequencyAllocator.java
+++ src/FrequencyAllocator.java
@@ -2,14 +2,16 @@ import java.util.*;
 
 public class FrequencyAllocator {
 
-    public boolean isFrequencySafe(Cell currentCell, Map<Cell, List<Cell>> network, int[] frequencies, int frequency, int numCells) {
+    public boolean isFrequencySafe(Cell currentCell, Map<Cell, List<Cell>> network, int currentFrequency) {
         List<Cell> closeCells = network.get(currentCell);
 
         for (Cell cell : closeCells) {
-            if (cell.frequency == currentCell.frequency) {
+            if (cell.frequency == currentFrequency) {
                 return false;
             }
         }
         return true;
     }
+
+    
 }

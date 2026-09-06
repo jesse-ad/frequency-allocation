19547ca7de076eaeedac6a45c2f7fa9ef96aeeee
Jesse <admjes003@myuct.ac.za>
2026-09-06 16:34:50 +0200

    Added method to check if its safe to assign frequency to a cell

--- src/FrequencyAllocator.java
+++ src/FrequencyAllocator.java
@@ -1,5 +1,15 @@
-
+import java.util.*;
 
 public class FrequencyAllocator {
-   
+
+    public boolean isFrequencySafe(Cell currentCell, Map<Cell, List<Cell>> network, int[] frequencies, int frequency, int numCells) {
+        List<Cell> closeCells = network.get(currentCell);
+
+        for (Cell cell : closeCells) {
+            if (cell.frequency == currentCell.frequency) {
+                return false;
+            }
+        }
+        return true;
     }
+}

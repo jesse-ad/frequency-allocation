f3ddc29d9b68e61197e7ae512f63e6211fde0d69
Jesse <admjes003@myuct.ac.za>
2026-09-06 19:03:39 +0200

    Added in allocation of frequencies method

--- src/FrequencyAllocator.java
+++ src/FrequencyAllocator.java
@@ -2,6 +2,14 @@ import java.util.*;
 
 public class FrequencyAllocator {
 
+    int bestCount = Integer.MAX_VALUE;
+    Map<String, Integer> bestAllocation = new HashMap<>();
+
+    public FrequencyAllocator(int bestCount) {
+        this.bestCount = bestCount;
+
+    }
+
     public boolean isFrequencySafe(Cell currentCell, Map<Cell, List<Cell>> network, int currentFrequency) {
         List<Cell> closeCells = network.get(currentCell);
 
@@ -13,5 +21,31 @@ public class FrequencyAllocator {
         return true;
     }
 
-    
+    public void allocateFrequencies(Cell[] cells, Map<Cell, List<Cell>> network, int currentIndex, int numCells, int[] frequencies) {
+        if (currentIndex == numCells) {
+            HashSet<Integer> frequenciesUsed = new HashSet<>();
+            for (Cell cell: cells) {
+                frequenciesUsed.add(cell.frequency);
+            }
+            if (bestCount >= frequenciesUsed.size()) {
+                bestCount = frequenciesUsed.size();
+
+                for (Cell cell : cells) {
+                    bestAllocation.put(cell.id, cell.frequency);
+                }
+            }
+        }
+
+        for (int i = 0; i < frequencies.length; i++) {
+            if (isFrequencySafe(cells[currentIndex], network, frequencies[i])) {
+                cells[currentIndex].frequency = frequencies[i];
+
+                allocateFrequencies(cells, network, currentIndex + 1, numCells, frequencies);
+            }
+            cells[currentIndex].frequency = 0;
+        }
+    }
 }
+
+
+
